eb85cad27ce2c3dad3c6568b699f9eaa46b4fb7f
isher <qiaoshuo@gmail.com>
2022-04-17 18:13:13 +0800

    Create ipecho.php

---
+++ ipecho.php
@@ -0,0 +1,34 @@
+<body topmargin='0' leftmargin='0'>
+<?php
+function getip() /*获取客户端IP*/
+{
+if (@$_SERVER["HTTP_X_FORWARDED_FOR"]){ //跳过代理获取客户端IP
+$ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+}
+else if (@$_SERVER["HTTP_CLIENT_IP"]){//直接获取获取客户端IP
+$ip = $_SERVER["HTTP_CLIENT_IP"];
+}
+else if (@$_SERVER["REMOTE_ADDR"]){
+$ip = $_SERVER["REMOTE_ADDR"];
+}
+else if (@getenv("HTTP_X_FORWARDED_FOR")){
+$ip = getenv("HTTP_X_FORWARDED_FOR");
+}
+else if (@getenv("HTTP_CLIENT_IP")){
+$ip = getenv("HTTP_CLIENT_IP");
+}
+else if (@getenv("REMOTE_ADDR")){
+$ip = getenv("REMOTE_ADDR");
+}
+else {
+$ip = "Unknown";
+}
+return $ip;
+}
+
+$ip=getIP(); //echo "var ip_address ='$ip'; ";
+
+echo $ip;
+
+?>
+</body>
